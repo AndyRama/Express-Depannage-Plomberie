3fc7c770fe39aa5d308710dc3bb92312ee178bfb
andy ramaroson <andyramaroson@gmail.com>
2024-10-11 09:41:19 +0200

    [setup] install base Test and create folder _test_ for jest

--- _test_/components/Page.test.jsx
+++ _test_/components/Page.test.jsx
@@ -5,8 +5,7 @@ import Home from '../page'
 describe('Page component', () => {
   test('renders an h1 with the text Home', () => { 
     render(<Home />) 
-    
-    // Vérifie si un <h1> avec le texte 'Home' est rendu
+  
     const headingElement = screen.getByRole('heading', { level: 1 })
     expect(headingElement).toHaveTextContent('Home')
   })
